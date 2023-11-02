126dfa692fbf2bd316043161af88e66de6af0e90
Dean <62970004+ShrimpHeavenNow@users.noreply.github.com>
2023-11-01 20:31:05 -0700

    Update MattDress.ino

--- MattDress.ino
+++ MattDress.ino
@@ -92,6 +92,12 @@ void setup() {
  for (int i = 0; i < 14; i++) {
    colorIndex[i] = random8();
  }
+
+ for(int x = 0; x < checkIf16(stripWeAreOn); x++){  //checks if an led is at 255 and if so, set it to the twinkle array.
+      Serial.println(x);
+      Serial.println("This is checking the checkif16");
+    }
+    delay(5000)
 }
 
 void loop() {
@@ -186,12 +192,7 @@ void stepOne() {
   
   if (allDone < 0){
 
-for(int x = 0; x < checkIf16(stripWeAreOn); x++){  //checks if an led is at 255 and if so, set it to the twinkle array.
-      Serial.println(x);
-      Serial.println("This is checking the checkif16");
-    }
-int q;
-cin >> q;
+
  
 
 Serial.println("we got to 1");
