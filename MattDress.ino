9e2e1b5ce5f10db5092c4721555e49f3ed32941e
Dean <62970004+ShrimpHeavenNow@users.noreply.github.com>
2023-11-01 19:05:13 -0700

    Update MattDress.ino

--- MattDress.ino
+++ MattDress.ino
@@ -1,7 +1,7 @@
 #include "FastLED.h"
 #include <OneButton.h>
 
-#define NUM_STRIPS 5
+#define NUM_STRIPS 4
 #define NUM_LEDS_PER_STRIP 10
 #define BTN_PIN   12
 #define RST_PIN   13
@@ -15,7 +15,7 @@ int pattern = 0;
 int wavePasssed = 0;
 bool firstOn = true;
 bool found = false;
-int currentIndex [] = {0,0,0,0,0,0,0,0,0,0}
+int currentIndex [] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 
 
 int fadingUpDumb[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
@@ -31,19 +31,17 @@ int fadingUp[10][2] = {
   {-1,0},
   {-1,0}
   } ; //the first value is the position, the second is the pallette index.
-int twinkling[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int twinkling[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int stripWeAreOn = 0;
 int doneStrips[] = {-1,-1,-1,-1,-1};
 int myBrainHurts = 0;
 
 
 
-CRGB leds[NUM_STRIPS][NUM_LEDS_PER_STRIP];
+CRGB leds[NUM_STRIPS][16]; //this might work?
 
 
-uint8_t colorIndex[NUM_LEDS_PER_STRIP];
-uint8_t colorIndex2[NUM_LEDS_PER_STRIP];
-uint8_t whichPalette = 0;
+uint8_t colorIndex[16];
 
 DEFINE_GRADIENT_PALETTE( redWave_gp ) {
   0,    0,  0,  0,     //Black
@@ -59,7 +57,6 @@ DEFINE_GRADIENT_PALETTE( smolder_gp ) {
 
 };
 
-CRGBPalette16 currentPalette(redWave_gp);
 CRGBPalette16 redWave(redWave_gp);
 CRGBPalette16 smolder(smolder_gp);
 
@@ -70,8 +67,7 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, 2>(leds[0], NUM_LEDS_PER_STRIP);
   FastLED.addLeds<NEOPIXEL, 3>(leds[1], NUM_LEDS_PER_STRIP);
   FastLED.addLeds<NEOPIXEL, 4>(leds[2], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 5>(leds[3], NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 6>(leds[4], NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 5>(leds[3], 16);
 
 
   FastLED.setBrightness(255);
@@ -87,7 +83,7 @@ void setup() {
   
 
   //Fill the colorIndex array with random numbers
- for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+ for (int i = 0; i < 16; i++) {
    colorIndex[i] = random8();
  }
 }
@@ -119,78 +115,81 @@ void loop() {
   FastLED.show();
 }
 
-void nextPattern(){
+void nextPattern(){ //this happend when the next button is pressed.
 
   pattern++;
 
 }
 
-int checkIf16(strip){ //This will check if we are on strip #4 and return 16.
-if (strip == 4){
-  return 16;
-} else {
-  return 10;  
-}
-}
+int checkIf16(strip){ //This will check if we are on strip #4 and return 16 if we are. 10 if we are not.
+  if (strip == 3){
+    return 16;
+  } else {
+    return 10;  
+    }
+  }
 
-void patOff() {
+void patOff() {  //This happens when the reset button is pressed
   reset = true;
   digitalWrite(RELAY_ONE_PIN, LOW);
   digitalWrite(RELAY_TWO_PIN, LOW);
 }
 
-void debug() {
+void debug() {  //this runs the first time we power on and after a reset and after power on until the next button is pressed.
 
-  if (firstOn == true){
+  if (firstOn == true){ //if it's the first time we are on, be blue for 1 second.
     Serial.print("First On!");
-for (int i = 0; i < NUM_STRIPS; i++){
-    for (int x = 0; x < NUM_LEDS_PER_STRIP; x++){
-      fill_solid(leds[i], NUM_LEDS_PER_STRIP, CRGB::Blue); //Just a Placeholder;
-
-          }
-        }
-delay(1000);
-firstOn = false;
-}
+      for (int i = 0; i < NUM_STRIPS; i++){
+          fill_solid(leds[i], checkIf16(i), CRGB::Blue); 
+            }
+     delay(1000);
+    firstOn = false; 
+    }
+    
   reset = false;
+  
   for (int i = 0; i < NUM_STRIPS; i++){
-    for (int x = 0; x < NUM_LEDS_PER_STRIP; x++){
-      fill_solid(leds[i], NUM_LEDS_PER_STRIP, CRGB::Black); //Just a Placeholder;
+    fill_solid(leds[i], checkIf16(i), CRGB::Black); //makes everything black.
 
-          }
-        }
-myBrainHurts = 0;
-stripWeAreOn= 0;
-        for( int x =0; x < NUM_LEDS_PER_STRIP; x++){
-          fadingUpDumb[x] = -1;
-          twinkling[x] = -1;
-          fadingUp[x][0] = -1;
-          fadingUp[x][-1] = 0;
-          Serial.print("we reset from debug");
-          }
-for (int x = 0; x < NUM_STRIPS; x++){
- doneStrips[x] = -1;
-}
-
-}
+    }
+        
+  myBrainHurts = 0; //starts resetting variables.
+  stripWeAreOn= 0;
+    
+  for( int x =0; x < NUM_LEDS_PER_STRIP; x++){
+            fadingUpDumb[x] = -1;
+            fadingUp[x][0] = -1;
+            fadingUp[x][-1] = 0;
+            Serial.print("we reset from debug");
+            }
+            
+  for (int i =0; i < 15; i++){
+    twinkling[i] = 0;
+    }
+          
+  for (int x = 0; x < NUM_STRIPS; x++){
+   doneStrips[x] = -1;
+    }
+ 
+  }
 
 void stepOne() {
 
-int allDone = 0;
-for( int x = 0; x < NUM_STRIPS; x++){
-allDone = allDone + doneStrips[x];
-}
-
-if (allDone < 0){
+  int allDone = 0;
+  for( int x = 0; x < NUM_STRIPS; x++){
+  allDone = allDone + doneStrips[x];
+  }
+  
+  if (allDone < 0){
  
   
-  EVERY_N_MILLISECONDS(500){ //Add a pixel to the list of pixels fading up.
+ EVERY_N_MILLISECONDS(500){ //Add a pixel to the list of pixels fading up.
     found = false;
     Serial.print("we got to the add pixel part");
-    for (int i = 0;  i < NUM_LEDS_PER_STRIP; i++){
+    for (int i = 0;  i < checkIf16(stripWeAreOn); i++){
       if (found == false){
         if (fadingUpDumb[i] == -1){
-          if (twinkling[i] == -1){
+          if (twinkling[i] == 0){
             fadingUpDumb[i] = i;
             fadingUp[i][0] =  i;  //adds it to the fading up list
             Serial.print("we got to the add pixel part farts farts farts");
@@ -202,54 +201,55 @@ if (allDone < 0){
     }
   }
 
-EVERY_N_MILLISECONDS(10){ //fade up each pixel that wants to fade up
-  //Serial.print("we got to the fade pixel part"); //confimed we get here
-  for(int x = 0; x < NUM_LEDS_PER_STRIP; x++){
-    if (fadingUp[x][0] != -1){
-      leds[stripWeAreOn][x] = ColorFromPalette( redWave, fadingUp[x][1]);
-      if(fadingUp[x][1] < 254){
-        fadingUp[x][1]++;
-        Serial.print("we got to the fade pixel part farts farts farts");
+  EVERY_N_MILLISECONDS(10){ //fade up each pixel that wants to fade up
+    //Serial.print("we got to the fade pixel part"); //confimed we get here
+    for(int x = 0; x < NUM_LEDS_PER_STRIP; x++){
+      if (fadingUp[x][0] != -1){
+        leds[stripWeAreOn][x] = ColorFromPalette( redWave, fadingUp[x][1]);
+        if(fadingUp[x][1] < 254){
+          fadingUp[x][1]++;
+          Serial.print("we got to the fade pixel part farts farts farts");
+        }
       }
     }
   }
-}
 
 
-  for(int x = 0; x < NUM_LEDS_PER_STRIP; x++){
+  for(int x = 0; x < NUM_LEDS_PER_STRIP; x++){  //checks if an led is at 255 and if so, set it to the twinkle array.
     if (fadingUp[x][1] == 255){
-      twinkling[x] = 0;
+      twinkling[x] = 1;
       Serial.print("we added soemthing to twinkle");
     }  //right now, the whole strip will stay at the end of red wave until the entire strip is there and then it will twinkle
  
     }
 
-//check if the whole strip is 255
-  
-mrBrainHurts = 0;
-for( int x = 0; x < NUM_LEDS_PER_STRIP; x++){
-mrBrainHurts = myBrainHurts + doneStrips[x];
-}
+    
+  mrBrainHurts = checkIf16(stripWeAreOn); this should have it be either 10 or 16
+  for( int x = 0; x < checkIf16(stripWeAreOn); x++){
+    mrBrainHurts = myBrainHurts + twinkle[x]; //if everything is twinkling, it should be either 10 or 16.
+    }
 
    
-      if (myBrainHurts < 0){ // if the strip is done with the wave, flag it as done, move onto the next strip, and reset the twinking and fading.
+      if (myBrainHurts == checkIf16(stripWeAreOn)){ // if the strip is done with the wave, flag it as done, move onto the next strip, and reset the twinking and fading.
         doneStrips[stripWeAreOn] = 0;
-        stripWeAreOn++;
-        myBrainHurts = 0;
+
         Serial.print("we reset and advanced");
-        for( int x =0; x < NUM_LEDS_PER_STRIP; x++){
+        for( int x =0; x < checkIf16(stripWeAreOn); x++){
           fadingUpDumb[x] = -1;
-          twinkling[x] = -1;
+          twinkling[x] = 0;
           fadingUp[x][0] = -1;
           fadingUp[x][-1] = 0;
-          
-          } 
+          }
+         stripWeAreOn++;
         }
-}
+  }
+
+  
+  
   // Color each pixel from the palette using the index from colorIndex[]
   for (int x = 0; x < NUM_STRIPS; x++){
     if (doneStrips[x] == 0)
-    for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+    for (int i = 0; i < checkIf16(x); i++) {
       leds[x][i] = ColorFromPalette(smolder, currentIndex[i]);
     }
   }
@@ -264,4 +264,3 @@ mrBrainHurts = myBrainHurts + doneStrips[x];
   }
   FastLED.show();
 }
-
