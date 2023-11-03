3616888087ac23e20d4991cf2cd7d6035635ad9b
Dean <62970004+ShrimpHeavenNow@users.noreply.github.com>
2023-11-02 22:26:19 -0700

    DONE AND CLEAN!

--- MattDress.ino
+++ MattDress.ino
@@ -17,10 +17,16 @@ bool firstOn = true;
 bool found = false;
 bool what = false;
 
-uint8_t currentIndex [] = {128,128,128,128,128,128,128,128,128,128,128,128,128,128};
+uint8_t currentIndex [5][14] = {
+  {128,128,128,128,128,128,128,128,128,128,128,128,128,128},
+  {128,128,128,128,128,128,128,128,128,128,128,128,128,128},
+  {128,128,128,128,128,128,128,128,128,128,128,128,128,128},
+  {128,128,128,128,128,128,128,128,128,128,128,128,128,128},
+  {128,128,128,128,128,128,128,128,128,128,128,128,128,128}
+  };
 
 
-int fadingUpDumb[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+int fadingUpDumb[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //Please don't jusge me. I'm a python boy and static arrays are scary.
 int fadingUp[14][2] = {
   {-1,0},
   {-1,0},
@@ -42,9 +48,12 @@ int stripWeAreOn = 0;
 int doneStrips[] = {-1,-1,-1,-1,-1,0};
 int myBrainHurts = 0;
 
+int veryStupid14[14] = {13,12,11,10,9,8,7,6,5,4,3,2,1,0} ; 
+int veryStupid10[10] = {9,8,7,6,5,4,3,2,1,0} ; 
 
 
-CRGB leds[NUM_STRIPS][14]; //this might work?
+
+CRGB leds[NUM_STRIPS][14]; 
 
 
 uint8_t colorIndex[14];
@@ -89,19 +98,13 @@ void setup() {
   pinMode(RELAY_TWO_PIN, OUTPUT);
   
 
-  //Fill the colorIndex array with random numbers
- for (int i = 0; i < 14; i++) {
+
+ for (int i = 0; i < 14; i++) {   //Fill the colorIndex array with random numbers
    colorIndex[i] = random8();
  }
-
-// for(int x = 0; x < checkIf16(stripWeAreOn); x++){  //checks if an led is at 255 and if so, set it to the twinkle array.
-//      Serial.println(x);
-//      Serial.println("This is checking the checkif16");
-//    }
-//    delay(5000);
 }
 
-void loop() {
+void loop() {    //checks what step we are on.
   
   if (reset == true){
     pattern = 0;
@@ -132,7 +135,19 @@ void nextPattern(){ //this happend when the next button is pressed.
 
 }
 
-int checkIf16(int strip){ //This will check if we are on strip #4 and return 16 if we are. 10 if we are not.
+int flippityDoo(int _number, int strip){
+  if (checkIf16(strip) == 14){
+    return (veryStupid14[_number]);
+
+  }else{
+    return (veryStupid10[_number]);
+  }
+
+
+
+}
+
+int checkIf16(int strip){ //This will check if we are on strip #5 and return 14 if we are. 10 if we are not.
   if (strip == 4){
     return 14;
   } else {
@@ -147,12 +162,12 @@ void patOff() {  //This happens when the reset button is pressed
   Serial.println("Reset Pressed.");
 }
 
-void debug() {  //this runs the first time we power on and after a reset and after power on until the next button is pressed.
+void debug() {  //This runs the first time we power on and after a reset and after power on until the next button is pressed.
 
-  if (firstOn == true){ //if it's the first time we are on, be blue for 1 second.
+  if (firstOn == true){    //if it's the first time we are on, be blue for .5 seconds.
     Serial.println("First On!");
       for (int i = 0; i < NUM_STRIPS; i++){
-      fill_solid(leds[i], checkIf16(i), CRGB::Blue); //makes everything black.
+      fill_solid(leds[i], checkIf16(i), CRGB::Blue); 
 
     }
     FastLED.show();
@@ -167,20 +182,23 @@ void debug() {  //this runs the first time we power on and after a reset and aft
 
     }
         
-  myBrainHurts = 0; //starts resetting variables.
+  myBrainHurts = 0; //starts resetting variables. (I think this one is no longer needed...)
   stripWeAreOn = 0;
     
   for( int x =0; x < 14; x++){
-            fadingUpDumb[x] = -1;
-            fadingUp[x][0] = -1;
-            fadingUp[x][-1] = 0;
-            twinkling[x] = 0;
-            //Serial.println("we reset from debug");
-            }
+    fadingUpDumb[x] = -1;
+    fadingUp[x][0] = -1;
+    fadingUp[x][-1] = 0;
+    twinkling[x] = 0;
+
+    }
             
           
   for (int x = 0; x < NUM_STRIPS; x++){
-   doneStrips[x] = -1;
+    doneStrips[x] = -1;
+    for(int i = 0; i < 14; i++){
+      currentIndex[x][i] = 128;
+      }
     }
   
   }
@@ -188,98 +206,59 @@ void debug() {  //this runs the first time we power on and after a reset and aft
 void stepOne() {
 
   int allDone = 0;
-  for( int x = 0; x < NUM_STRIPS; x++){
+  for( int x = 0; x < NUM_STRIPS; x++){  //Checks if all the strips are done. Skips it if they are.
   allDone = allDone + doneStrips[x];
   }
   if (allDone < 0){
 
 
- 
-
-//Serial.println("we got to 1");
      
- EVERY_N_MILLISECONDS(500){ //Add a pixel to the list of pixels fading up.
-    Serial.println("Strip we are on:");
-    Serial.println(stripWeAreOn);
-    Serial.println(checkIf16(stripWeAreOn));
-    Serial.println("");
-    Serial.println("Done Strips:");
-    for (int x = 0; x < NUM_STRIPS; x++){
-      Serial.println(doneStrips[x]);
-    }
+ EVERY_N_MILLISECONDS(100){ //Add a pixel to the list of pixels fading up. Changing this time makes the wave advance faster or slower.
     
+    found = false; //Look, this FEELS not needed, but here it is.
+    for (int i = 0 ;  i < checkIf16(stripWeAreOn); i++){
 
 
+      int x = flippityDoo(i, stripWeAreOn); 
 
-    found = false;
-    for (int i = 0;  i < checkIf16(stripWeAreOn); i++){
+
       if (found == false){
-        if (fadingUpDumb[i] == -1){
-          if (twinkling[i] == 0){
-            fadingUpDumb[i] = 0;
-            fadingUp[i][0] =  0;  //adds it to the fading up list
-            Serial.println("we added");
-            Serial.println(i);
-            Serial.println(""); 
+        if (fadingUpDumb[x] == -1){ //Is this already in the fading list?
+          if (twinkling[x] == 0){ //Is this already in the twinkling list?
+            fadingUpDumb[x] = 0;
+            fadingUp[x][0] =  0;  //adds it to the fading up list
             found = true;
             }
           }
 
         }
     }
-    
   }
 
-//Serial.println("we got to 2");
 
-  EVERY_N_MILLISECONDS(10){ //fade up each pixel that wants to fade up
-    if (stripWeAreOn == 4){
-        Serial.println("4 10 and 14 are:");
-        Serial.println(fadingUp[9][1]);
-        Serial.println(fadingUp[13][1]);
-        Serial.println("");
-    }
-  
+
+  EVERY_N_MILLISECONDS(1){ //fade up each pixel that wants to fade up. Changing this time makes the pixels go through the pallet faster or slower.
 
     for(int x = 0; x < checkIf16(stripWeAreOn); x++){
-      if (fadingUp[x][0] == 0){
-        leds[stripWeAreOn][x] = ColorFromPalette( redWave, fadingUp[x][1]);
-        if(fadingUp[x][1] < 254){
-          fadingUp[x][1]++;
+      int i = flippityDoo(x, stripWeAreOn);
+      if (fadingUp[i][0] == 0){
+        leds[stripWeAreOn][i] = ColorFromPalette( redWave, fadingUp[i][1]);
+        if(fadingUp[i][1] < 254){
+          fadingUp[i][1]++;
           }else{
-            twinkling[x] = 1;
-            fadingUp[x][0] = -1;
-            Serial.println("Strip:");
-            Serial.println(stripWeAreOn);
-            Serial.println(x);
-            Serial.println("is at 255");
-            Serial.println("");
-            //Serial.println("we got to the fade pixel part farts farts farts");
+            twinkling[i] = 1;
+            fadingUp[i][0] = -1;
+
         }
       }
     }
   }
-
-
-//Serial.println("we got to 3");
-
-//  for(int x = 0; x < checkIf16(stripWeAreOn); x++){  //checks if an led is at 255 and if so, set it to the twinkle array.
-//    if (fadingUp[x][1] == 255){
-//      twinkling[x] = 1;
-//      Serial.println(x);
-//      Serial.println("we added soemthing to twinkle");
-//    }  //right now, the whole strip will stay at the end of red wave until the entire strip is there and then it will twinkle
-// 
-//    }
-
-//Serial.println("we got to 4");
-    
+   
   int myBrainHurts = 0; //this should have it be either 10 or 16
   for( int x = 0; x < checkIf16(stripWeAreOn); x++){
     myBrainHurts = myBrainHurts + twinkling[x]; //if everything is twinkling, it should be either 10 or 14.
     }
 
-  
     if (myBrainHurts == checkIf16(stripWeAreOn)){ // if the strip is done with the wave, flag it as done, move onto the next strip, and reset the twinking and fading.
       what = true;
       Serial.println("we reset and advanced");
@@ -290,39 +269,46 @@ void stepOne() {
         fadingUp[x][0] = -1;
         fadingUp[x][-1] = 0;
         }
-
-
-        Serial.println("New Strip:");
-        Serial.println(stripWeAreOn);
       }
-  if(what == true){
-    Serial.println("this strip is beign added to done:");
-    Serial.println(stripWeAreOn);
+
+  if(what == true){  //Look, I KNOW this is stupid, but there was some really weird behavior and this helped debbug and now if it ain't broke, dont fix it.
     doneStrips[stripWeAreOn] = 0;
     stripWeAreOn++;
     what = false;  
   }
 }
 
- // Serial.println("we got to 5");
-  
-  // Color each pixel from the palette using the index from colorIndex[]
-  for (int x = 0; x < 5; x++){
-    if (doneStrips[x] == 0)
-    for (int i = 0; i < checkIf16(x); i++) {
-      leds[x][i] = ColorFromPalette(smolder, currentIndex[i]);
-    }
+//Below here is the smouldering stuff.
+
+for (int x = 0; x < 5; x++){
+  if (doneStrips[x] == 0)
+  for (int i = 0; i < checkIf16(x); i++) {
+    leds[x][i] = ColorFromPalette(smolder, currentIndex[x][i]);
   }
-  EVERY_N_MILLISECONDS(10){
-    for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-      colorIndex[i]++;
-      currentIndex[i]++;
-      
+}
+EVERY_N_MILLISECONDS(10){  //Changing this time makes the smoulder faster or slower.
+  
+  for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+    colorIndex[i]++;
+    for (int x  = 0; x < NUM_STRIPS; x++){
+      if (doneStrips[x] == 0){
+        
+        currentIndex[x][i]++;
+      }
     }
-   EVERY_N_MILLISECONDS(50){
+    
+    
+  }
+
+   EVERY_N_MILLISECONDS(80){  //Changing this time changes how quickly the strip moved into the smouldering effect after the wave.
     for (int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
-    if(currentIndex[i] != colorIndex[i]){
-        currentIndex[i]++; //This is a dirty trick to get the smoulder to not jump to the index value, but fade to it.
+      for (int x  = 0; x < NUM_STRIPS; x++){
+        if (doneStrips[x] == 0){
+          if(currentIndex[x][i] != colorIndex[i]){
+            currentIndex[x][i]++; //This is a dirty trick to get the smoulder to not jump to the index value, but fade to it.
+        }
+      }
+    
       }
    }
    }
